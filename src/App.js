a654a901df2d455ec512d18104bc68c7f4b9e849
Danny Gavin <homevpn14@gmail.com>
2021-05-17 11:59:13 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,99 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import { Route, Link, Switch } from "react-router-dom";
 import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+
+  // URL in a variable
+  const url = "http://localhost:4500"
+
+  // State to hold the list of dogs
+  const [dogs, setDogs] = useState([])
+
+  // GET the dogs
+  const getDogs = () => {
+    fetch(url + "/dog/")
+    .then((response) => response.json())
+    .then((data) => {
+      setDogs(data)
+    })
+  }
+
+  useEffect(() => {getDogs()}, [])
+
+  //handleCreate - function for when the create form is submitted
+  const handleCreate = (newDog) => {
+    fetch(url + "/dog/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newDog)
+    })
+    .then(() => getDogs())
+  }
+
+  //handleUpdate - function for when the edit form is submitted
+  const handleUpdate = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dog)
+    })
+    .then(() => getDogs())
+  }
+
+  //deleteDog to delete individual dogs
+  const deleteDog = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "delete"
+    })
+    .then(() => {
+      getDogs()
+    })
+  }
+
+  // Empty Dog - For the Create Form
+  const emptyDog = {
+    name: "",
+    age: 0,
+    img: ""
+  }
+
+  const [selectedDog, setSelectedDog] = useState(emptyDog)
+    
+  //function to specify which dog we are updating
+    const selectDog = (dog) => {
+      setSelectedDog(dog)
+    }
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to="/create">
+        <button>Add Dog</button>
+      </Link>
       <main>
         <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+          <Route exact path="/" render={(rp) => 
+            <Display {...rp} dogs={dogs} selectDog={selectDog}  deleteDog={deleteDog} />} />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="create" dog={emptyDog} handleSubmit={handleCreate} />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="update" dog={selectedDog} handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  // destruct the dogs from props
+  const { dogs } = props
+
+  const loaded = () => {
+    return  <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => {
+        return (
+          <article key={dog._id}>
+            <img src={dog.img} />
+            <h1>{dog.name}</h1>
+            <h3>{dog.age}</h3>
+            <button onClick={() => {
+              props.selectDog(dog)
+              props.history.push("/edit") //this just opens the /edit page (.history.push is literally a link, thats all it does, it opens a new)
+            }}>Edit</button>
+            <button onClick={() => {props.deleteDog(dog)}}>Delete</button>
+          </article>
+        )
+      })}
+    </div>
+  }
+
+  const loading = () => {
+    return <h1>Loading</h1>
+  }
+  
+  return dogs.length > 0 ? loaded() : loading()
+
 };
 
 export default Display;
